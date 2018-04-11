b41ae11ba5cb55d128b389a2de3a2cebc38fdc45
larperdoodle <larsbenedetto@gmail.com>
2018-04-11 10:28:58 -0700

    first commit

---
+++ app/src/main/java/com/ibisrecycling/lars/pickupscheduler/PickupsActivity.kt
@@ -0,0 +1,11 @@
+package com.ibisrecycling.lars.pickupscheduler
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PickupsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pickups)
+    }
+}
