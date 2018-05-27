6dffafc4080272193fe2df568d92e6467a19bce5
larperdoodle <larsbenedetto@gmail.com>
2018-05-27 15:24:42 -0700

    Add untested tsp solver

--- Flutter/pickup_scheduler/lib/utils/TravellingTrashmanSolver.dart
+++ Flutter/pickup_scheduler/lib/utils/TravellingTrashmanSolver.dart
@@ -1,34 +1,109 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:http/http.dart' as http;
 import 'package:pickup_scheduler/utils/Customer.dart';
 
 class TravellingTrashmanSolver {
-  static Future<String> getOptimalRoute(Point start, List<Customer> group, Point end) {
+  static String getMatrixUrl(Point start, List<Customer> group, Point end) {
     String distanceMatrix = "https://maps.googleapis.com/maps/api/distancematrix/json";
     String locations = "${start.x},${start.y}";
     for (Customer c in group) {
       locations += "|${c.x},${c.y}";
     }
-
+    locations += "|${end.x},${end.y}";
     distanceMatrix += "?origins=" + locations;
     distanceMatrix += "?destinations=" + locations;
+    return distanceMatrix;
+  }
 
-    return http.get(distanceMatrix).then((response) {
-      List<Point> sortedGroup = new List<Point>();
-      List<List<int>> matrix = new List<List<int>>();
-      for (int from = 0; from < group.length; from++) {
-        for (int to = 0; to < group.length; to++) {
+  static Future<String> getOptimalRoute(final Point start, List<Customer> group, final Point end) async {
+    List<Point> points = new List<Point>();
+    points.add(start);
+    for (Customer c in group) {
+      points.add(new Point(c.x, c.y));
+    }
+    points.add(end);
 
-        }
+    final response = await http.get(getMatrixUrl(start, group, end));
+    final responseJson = json.decode(response.body);
+
+    List<List<Node>> matrix = new List<List<Node>>();
+
+    for (int from = 0; from < group.length; from++) {
+      for (int to = 0; to < group.length; to++) {
+        matrix[from][to] = new Node(points[to].x, points[to].y, responseJson["rows"][from]["elements"][to]["duration"]["value"]);
       }
-      //This should work as long as there are less than 23 customers in the group
-      String mapsURL = "https://www.google.com/maps/dir/";
-      for (Customer c in group) {
-        mapsURL += "${c.x},${c.y}/";
+    }
+    Path startPath = new Path();
+    startPath.add(matrix[0][0], 0);
+    Path optimalPath = getOptimalPath(matrix, 0, startPath);
+
+    //This should work as long as there are less than 23 customers in the group
+    String mapsURL = "https://www.google.com/maps/dir/";
+    for (Node n in optimalPath.path) {
+      mapsURL += "${n.x},${n.y}/";
+    }
+    return mapsURL;
+  }
+
+  static Path getOptimalPath(List<List<Node>> matrix, int from, Path p) {
+    List<Path> paths = new List<Path>();
+    List<Node> nodes = matrix[from];
+    for (int to = 0; to < nodes.length; to++) {
+      if (p.visitedNodes.contains(to)) continue; //Already visited this node
+      if (to == nodes.length - 1 && p.length != nodes.length - 1) continue; //Not yet ready to visit end node
+      Path path = p.duplicate();
+      path.add(matrix[from][to], to);
+      paths.add(getOptimalPath(matrix, to, path));
+    }
+    Path shortest = paths[0];
+    for (Path p in paths) {
+      if (p.time < shortest.time) {
+        shortest = p;
       }
-      return mapsURL;
-    });
+    }
+    return shortest;
+  }
+}
+
+class Node {
+  double x;
+  double y;
+  int time;
+
+  Node(this.x, this.y, this.time);
+}
+
+class Path {
+  List<int> visitedNodes;
+  List<Node> path;
+  int time;
+  int length;
+
+  Path() {
+    path = new List<Node>();
+    time = 0;
+    length = 0;
+  }
+
+  add(Node n, int ix) {
+    path.add(n);
+    time += n.time;
+    length++;
+    visitedNodes.add(ix);
+  }
+
+  duplicate() {
+    Path newPath = new Path();
+    for (Node n in path) {
+      newPath.add(n, 0);
+    }
+    newPath.visitedNodes = new List<int>();
+    for (int i in visitedNodes) {
+      newPath.visitedNodes.add(i);
+    }
+    newPath.time = time;
   }
 }
